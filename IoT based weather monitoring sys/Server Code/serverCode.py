aa43b1a718e43c972491ee1e95ff4835b99c3f71
OnFim25 <156911603+OnFim25@users.noreply.github.com>
2024-05-27 15:29:42 +0530

    commented the server code

--- IoT based weather monitoring sys/Server Code/serverCode.py
+++ IoT based weather monitoring sys/Server Code/serverCode.py
@@ -1,23 +1,24 @@
 from flask import Flask, request, jsonify, render_template
-import mongoengine as me
-from datetime import datetime
+import mongoengine as me    #mongoengine is a library used to access data from mongoDB
+from datetime import datetime   
 
 
 
-app = Flask(__name__)
+app = Flask(__name__)   
 
-dbs = me.connect(host = "mongodb+srv://neerajpcvr581:neeraj1981998@weathermonitoring.r9zztni.mongodb.net/?retryWrites=true&w=majority&appName=weatherMonitoring")
+#to connect the database on mongoDB
+me.connect(host = "mongodb+srv://neerajpcvr581:neeraj1981998@weathermonitoring.r9zztni.mongodb.net/?retryWrites=true&w=majority&appName=weatherMonitoring")
 
-
+#create the json file in the database
 class weather_data(me.Document):
     time = me.StringField(default="")
     temp = me.StringField(default="")
     humi = me.StringField(default="")
 
-#This route is acessed by the microcontroller only
+#This API route is acessed by the microcontroller only to upload temprature and humidity data through GET request
 @app.route("/upload_data", methods=["get"]) 
 def upload_data():
-
+    #To get system time to add to the data base
     now = datetime.now()
     current_time = now.strftime("%Y-%m-%d %H:%M:%S")
     print(current_time)
@@ -27,22 +28,27 @@ def upload_data():
     humi = request.args.get("humi")
     print("microcontroller acessed")
     print(f"{time}=={temp}=={humi}")
-    new_data = weather_data( #new_data is an object of weather_data
+
+    #new_data is an object of weather_data
+    new_data = weather_data( 
         time=time,
         temp=temp,
         humi=humi
     )
     
     new_data.save() #to save the new_data in the database
-    return "" # all api must have a return value
+    return ""   # all api must have a return value
 
 #This route is for accessing the data on database and displaying it on an html webpage
+#The HTML file should be inside "template" folder inside the code folder
 @app.route("/show_data", methods=["get"]) 
 def show_data():
     data = weather_data.objects()
     print(data)
     return render_template("list_data.html", new_data = data)
 
+#This API deletes all data inside the database
+#This route recives POST request from the "Clear All" button on the webpage
 @app.route("/clearAll", methods = ["post"])
 def clear_all():
     data = weather_data.objects()
@@ -54,4 +60,5 @@ def clear_all():
     return render_template("list_data.html", new_data = data)
 
 
-app.run(debug=True, host = "0.0.0.0")
+app.run(debug=True, host = "0.0.0.0")   #host = 0.0.0.0 to recive request from any device on the network
+                                        #debug = True for debuging purposes
